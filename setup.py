2bcb2438db1783791e2569f09e5a6de7c9f44ccc
Charles R. Whealton (Chuck) <33600254+chuckwhealton@users.noreply.github.com>
2021-01-08 21:03:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as fh:
 
 setup(
     name='pps_gui',  # Required
-    version='2.26',  # Required
+    version='2.025',  # Required
     description='A software based Raspberry Pi type simulator to aid in teaching Python, requires pps_emu.',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
